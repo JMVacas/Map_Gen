dd9e0fe4b5e0f01528f676ca350049635aa2c342
Jorge Martin <jorgemartincm@gmail.com>
2018-10-01 09:43:22 +0200

    Agregar archivos de proyecto.

---
+++ Map_Gen/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Map_Gen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 33);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(96, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 68);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Map_Gen/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Map_Gen
+{
+    public partial class Form1 : Form
+    {
+        string Mapa;
+        string Texto;
+        string Nombre_Variable;
+        List<string> Lista_Mapa = new List<string>();
+        bool Error_Tamaño;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Nombre_Variable = textBox1.Text;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ArchivoCSV = new OpenFileDialog();
+            ArchivoCSV.Filter = "Archivos csv|*.csv";
+            ArchivoCSV.Title = "Mapa de puntos ";
+
+            if (ArchivoCSV.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader Lector = new StreamReader(ArchivoCSV.FileName);
+                Mapa=Lector.ReadToEnd();
+                Mapa=Mapa.Replace(";", "");
+                Mapa=Mapa.Replace("\n", "");
+                Mapa = Mapa.Replace("\t", "");
+
+
+
+                Lista_Mapa = Mapa.Split('\r').ToList();
+                
+                
+                int Tamanio = Lista_Mapa[0].Length;
+                for (int i = 0; i<Lista_Mapa.Count; i++)
+                {
+                    for (int j = 0; j < Lista_Mapa[i].Length; j++)
+                    {
+                        if (Lista_Mapa[i].Length != Tamanio)
+                        {
+                            i = Lista_Mapa.Count;
+                        }
+                        else if (Lista_Mapa[i][j] >'4' || Lista_Mapa[i][j]<'0')
+                        {
+
+                        }
+                        else
+                        {
+                            Texto += Nombre_Variable +"[" + (i+1) + "," + (j+1) + "]:= " + Lista_Mapa[i][j] + "; \r\n";
+                        }
+                    }
+                }
+                Lector.Close();
+            }
+
+            SaveFileDialog Text_OUT = new SaveFileDialog();
+            Text_OUT.Filter = "Archivos txt |*.txt";
+            Text_OUT.Title = "Mapa Texto";
+            if (Text_OUT.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter Escritor = new StreamWriter(Text_OUT.FileName);
+                Escritor.Write(Texto);
+                Escritor.Close();
+            }
+        }
+
+        
+    }
+}
